079f425a045131b7b947211e5f0e2f66abb5a5d4
bspippi1337 <bspippi1337@gmail.com>
2026-02-24 04:14:17 +0100

    Restless v2 architecture cleanup + repo normalization

--- internal/adapters/http/transport.go
+++ internal/adapters/http/transport.go
@@ -1,35 +1,35 @@
 package httpadapter
 
 import (
-    "bytes"
-    "context"
-    "io"
-    "net/http"
-    "github.com/bspippi1337/restless/internal/core/engine"
+	"bytes"
+	"context"
+	"github.com/bspippi1337/restless/internal/core/engine"
+	"io"
+	"net/http"
 )
 
 type HTTPTransport struct{}
 
 func (t *HTTPTransport) Do(ctx context.Context, job engine.Job) engine.Result {
-    req, err := http.NewRequestWithContext(ctx, job.Method, job.Target, bytes.NewReader(job.Body))
-    if err != nil {
-        return engine.Result{Err: err}
-    }
+	req, err := http.NewRequestWithContext(ctx, job.Method, job.Target, bytes.NewReader(job.Body))
+	if err != nil {
+		return engine.Result{Err: err}
+	}
 
-    for k, v := range job.Headers {
-        req.Header.Set(k, v)
-    }
+	for k, v := range job.Headers {
+		req.Header.Set(k, v)
+	}
 
-    resp, err := http.DefaultClient.Do(req)
-    if err != nil {
-        return engine.Result{Err: err}
-    }
-    defer resp.Body.Close()
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return engine.Result{Err: err}
+	}
+	defer resp.Body.Close()
 
-    body, _ := io.ReadAll(resp.Body)
+	body, _ := io.ReadAll(resp.Body)
 
-    return engine.Result{
-        Status: resp.StatusCode,
-        Body:   body,
-    }
+	return engine.Result{
+		Status: resp.StatusCode,
+		Body:   body,
+	}
 }

7e3ab66412c1fb2ac46ae1283e19ae9148bea0ad
bspippi1337 <bspippi1337@gmail.com>
2026-02-24 11:00:37 +0100

    feat: promote v2 as primary CLI (restless)

--- cmd/restless/main.go
+++ cmd/restless/main.go
@@ -1,10 +1,78 @@
 package main
 
 import (
-	"github.com/bspippi1337/restless/internal/ui/cli"
+	"context"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/bspippi1337/restless/internal/core/app"
+	"github.com/bspippi1337/restless/internal/core/types"
+	"github.com/bspippi1337/restless/internal/modules/bench"
+	"github.com/bspippi1337/restless/internal/modules/export"
+	"github.com/bspippi1337/restless/internal/modules/openapi"
+	"github.com/bspippi1337/restless/internal/modules/session"
 )
 
 func main() {
-	root := cli.NewRootCmd()
-	root.Execute()
+
+	if len(os.Args) > 1 {
+		switch os.Args[1] {
+		case "openapi":
+			handleOpenAPI(os.Args[2:])
+			return
+		case "profile":
+			handleProfile(os.Args[2:])
+			return
+		}
+	}
+
+	runRequestMode(os.Args[1:])
+}
+
+func runRequestMode(args []string) {
+	fs := flag.NewFlagSet("request", flag.ExitOnError)
+
+	method := fs.String("X", "GET", "HTTP method")
+	url := fs.String("url", "", "Request URL")
+	body := fs.String("d", "", "Body string")
+
+	fs.Parse(args)
+
+	if *url == "" {
+		fmt.Println("missing -url")
+		fs.Usage()
+		os.Exit(1)
+	}
+
+	sess := session.New()
+	mods := []app.Module{
+		sess,
+		openapi.New(),
+		export.New(),
+		bench.New(),
+	}
+
+	a, err := app.New(mods)
+	if err != nil {
+		fmt.Println("ERROR:", err)
+		os.Exit(1)
+	}
+
+	req := types.Request{
+		Method:  *method,
+		URL:     *url,
+		Headers: http.Header{},
+		Body:    []byte(*body),
+	}
+
+	resp, err := a.RunOnce(context.Background(), req)
+	if err != nil {
+		fmt.Println("ERROR:", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("status: %d (dur=%dms)\n", resp.StatusCode, resp.DurationMs)
+	fmt.Println(string(resp.Body))
 }

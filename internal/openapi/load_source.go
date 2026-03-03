635559ba086f641e5337232d32b325e827157a1c
bspippi1337 <bspippi1337@gmail.com>
2026-03-04 00:31:31 +0100

    openapi: robust HTTP loader (detect HTML responses)

--- internal/openapi/load_source.go
+++ internal/openapi/load_source.go
@@ -1,8 +1,10 @@
 package openapi
 
 import (
+	"errors"
 	"io"
 	"net/http"
+	"os"
 	"strings"
 )
 
@@ -10,15 +12,27 @@ func loadSource(src string) ([]byte, error) {
 
 	if strings.HasPrefix(src, "http://") || strings.HasPrefix(src, "https://") {
 
-		resp, err := http.Get(src)
+		client := &http.Client{}
+
+		resp, err := client.Get(src)
 		if err != nil {
 			return nil, err
 		}
 
 		defer resp.Body.Close()
 
-		return io.ReadAll(resp.Body)
+		data, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+
+		# detect HTML response
+		if strings.HasPrefix(strings.TrimSpace(string(data)), "<") {
+			return nil, errors.New("received HTML instead of OpenAPI spec")
+		}
+
+		return data, nil
 	}
 
-	return loadSource(src)
+	return os.ReadFile(src)
 }

977d730af2f63067a9f7a1d0695f69ace1502b5a
bspippi1337 <bspippi1337@gmail.com>
2026-03-04 00:23:29 +0100

    v6 discovery: swagger/openapi autodiscovery

---
+++ internal/discovery/scan.go
@@ -0,0 +1,34 @@
+package discovery
+
+import (
+	"net/http"
+)
+
+var candidates = []string{
+	"/swagger.json",
+	"/openapi.json",
+	"/v3/api-docs",
+	"/api-docs",
+	"/swagger/v1/swagger.json",
+}
+
+func Find(base string) (string, bool) {
+
+	for _, p := range candidates {
+
+		u := base + p
+
+		resp, err := http.Get(u)
+		if err != nil {
+			continue
+		}
+
+		resp.Body.Close()
+
+		if resp.StatusCode == 200 {
+			return u, true
+		}
+	}
+
+	return "", false
+}

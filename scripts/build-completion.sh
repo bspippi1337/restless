80c338649904887f13463aab7aad0d78b3e7376b
bspippi1337 <bspippi1337@gmail.com>
2026-03-03 21:52:29 +0100

    chore(apt): signed multi-arch repo 5.2.0-gnu-manual-3-gdb1e913

--- scripts/build-completion.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-go build -o /tmp/restless ./cmd/restless
-/tmp/restless completion bash > docs/completion/restless.bash
-/tmp/restless completion zsh > docs/completion/_restless
-/tmp/restless completion fish > docs/completion/restless.fish
-echo "Completion generated."

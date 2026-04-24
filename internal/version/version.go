4b0969b2000386a59c18a1605b2aac8b0e955ac5
bspippi1337 <bspippi1337@gmail.com>
2026-04-24 22:15:05 +0200

    Make version metadata release-build friendly

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,3 +1,24 @@
 package version
 
-var Version = "4.0.4"
+import (
+	"fmt"
+	"runtime"
+)
+
+var (
+	Version = "0.4.0"
+	Commit  = "dev"
+	Date    = "unknown"
+)
+
+func Short() string {
+	return Version
+}
+
+func String() string {
+	return fmt.Sprintf("restless %s (%s %s)", Version, Commit, Date)
+}
+
+func Details() string {
+	return fmt.Sprintf("restless %s\ncommit: %s\nbuilt: %s\ngo: %s\nplatform: %s/%s\n", Version, Commit, Date, runtime.Version(), runtime.GOOS, runtime.GOARCH)
+}

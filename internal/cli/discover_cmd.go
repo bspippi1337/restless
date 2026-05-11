a148129db75e0cf4024b682cfd6044dabfdf3938
blckswan1337 <bspippi1337@gmail.com>
2026-05-11 22:20:55 +0200

    restless: replace legacy discover cmd. (2026-05-11 22:20:55)

--- internal/cli/discover_cmd.go
+++ internal/cli/discover_cmd.go
@@ -2,58 +2,24 @@ package cli
 
 import (
 	"fmt"
-	"time"
 
+	"github.com/bspippi1337/restless/internal/discoverwow"
 	"github.com/spf13/cobra"
-
-	"github.com/bspippi1337/restless/internal/discovery"
-	"github.com/bspippi1337/restless/internal/ui"
 )
 
 func NewDiscoverCmd() *cobra.Command {
-
 	cmd := &cobra.Command{
-
-		Use:   "discover <url>",
-		Short: "reverse engineer an API",
-
+		Use:   "discover [target]",
+		Short: "Semantic API discovery engine",
+		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-
-			target := args[0]
-
-			ui.Banner()
-
-			ui.Start()
-
-			fmt.Println("target:", target)
-			fmt.Println()
-
-			start := time.Now()
-
-			res, err := discovery.Discover(target)
-			if err != nil {
-				return err
-			}
-			_ = res.Endpoints
+			res, err := discoverwow.Discover(args[0])
 			if err != nil {
 				return err
 			}
 
-			elapsed := time.Since(start)
-
-			fmt.Println()
-			fmt.Println()
-			fmt.Println("discovered endpoints:", len(res.Endpoints))
-			fmt.Println("scan time:", elapsed)
-
-			for _, e := range res.Endpoints {
-
-				fmt.Println(" ", e)
-
-			}
-
+			fmt.Print(discoverwow.Render(res))
 			return nil
-
 		},
 	}
 
